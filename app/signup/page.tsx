803834a1a6f049ed1e989a873e24a6236ae0b062
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-07-19 23:57:41 +0530

    react-hot-toast

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 import Link from 'next/link';
 import React,{ useState,useEffect } from 'react'
+import toast, { Toaster } from "react-hot-toast";
 
 function SignUp () {
+  const notify = (msg:string) => {
+    toast.success(`${msg}`)
+  }
   const [signup,setSignup] = useState<boolean>(false);
   const [disableButton,setDisableButton] = useState(true);
   // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -29,12 +33,13 @@ function SignUp () {
   };
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
-    alert('sumbmitted')
+    // alert('sumbmitted')
     e.preventDefault();
     setSignup(true);
   };
   return (
     <div className="text-center justify-center items-center mt-40">
+      <Toaster position='bottom-left' reverseOrder={false}/>
       <h1 className="text-3xl font-bold p-2">Signup</h1>
       <form
         action="#"
@@ -79,10 +84,14 @@ function SignUp () {
 
         <div className="text-center">
           <button
-          onClick={(e)=>handleSubmit(e)}
+          // onClick={(e)=>{handleSubmit(e)};notify('done');}
+          onClick={(e)=>{
+            handleSubmit(e);
+            notify('Submitted');
+          }}
             type="submit"
             // className="hover:bg-green-600 bg-green-400 p-3 font-medium text-center rounded text-md"
-            className={`${signup==true?"hover:bg-green-600":'hover:bg-red-600'} bg-green-400 p-3 font-medium text-center rounded text-md`}
+            className={`hover:bg-green-600 bg-green-400 p-3 font-medium text-center rounded text-md`}
             disabled={disableButton}
           >
             Submit
