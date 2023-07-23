c8d8fffc8c22dbe94275e8dd2b88edde45dff74f
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-07-23 22:33:20 +0530

    retrieving user details from cookies

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
-const Profile = ({params}:any) => {
+const Profile = ({ params }: any) => {
   return (
-    <div className="text-center font-bold text-3xl m-40">
-      id : <span  className='bg-slate-900 text-white font-bold p-2 rounded'> {params.id} </span>
+    <div>
+      <div className="text-center font-bold text-3xl m-40">
+        id :{" "}
+        <span className="bg-slate-900 text-white font-bold p-2 rounded">
+          {" "}
+          {params.id}{" "}
+        </span>
+        <br />
+      </div>
+      <button className="text-center mx-50 bg-green-400 rounded p-4">
+        <Link href={"/profile"}>Dashboard</Link>
+      </button>
     </div>
   );
-}
+};
 
-export default Profile
+export default Profile;
