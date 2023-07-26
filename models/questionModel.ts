c8391b8212e4e0dac9dbbfe2bdaad103cca17ac2
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-07-26 23:56:05 +0530

    question model

---
+++ models/questionModel.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const questionScheme = new mongoose.Schema({
+    title:{
+        type: 'string',
+        required: true,
+    },
+    number:{
+        type: 'number',
+        required: true,
+    },
+    description: {
+        type: 'string',
+        required: true,
+    }
+})
+
+const Question = mongoose.models.questions || mongoose.model("questions", questionScheme);
+
+export default Question
