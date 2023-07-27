f4958bf3a863e49ef99585a51008e1e87665cbe3
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-07-28 01:09:43 +0530

    api/question get

--- app/problems/page.tsx
+++ app/problems/page.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+"use client"
+import React from "react";
+import axios from "axios";
+import { useState,useEffect } from "react";
 
-const Problems = () => {
-  return (
-    <div className='mt-30 space-x-48 text-center font-bold'>
-      <p>Problems</p>
-    </div>
-  )
+interface ProblemType {
+  title:string;
+  description:string;
+  number:number;
 }
 
-export default Problems
+const Problems = () => {
+  const [data,setData] = useState<ProblemType>()
+  let ps:ProblemType|[] = []
+  const perPage = 1;
+  const getProblems = async () => {
+    console.log('data')
+    try {
+      const d = await axios.post("/api/problem/get", { perPage });
+      ps = d.data.data
+      console.log(ps)
+      // setData(data?.title:d.data.title)
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+    useEffect(()=>{
+      getProblems()
+    },[])
+
+  return (
+    <div>
+      <div className="m-48 text-center text-3xl font-bold">
+        <p>Problems</p>
+      </div>
+      <div className="text-center">
+        {/* {ps.length>0 && ps?.map((problem,ind)=>{
+          <div key={ind}></div>
+        })} */}
+      </div>
+    </div>
+  );
+};
+
+export default Problems;
