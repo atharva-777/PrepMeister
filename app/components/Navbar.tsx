b542ba73825abb888c2837edcbb18a05dc32a7e4
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-07-29 01:59:09 +0530

    problems table-fetching data / slugify

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,26 +3,40 @@ import React from "react";
 import Link from "next/link";
 import { useContext } from "react";
 import { UserContext } from "../context/UserProvider";
+import { TbUserCircle } from "react-icons/tb";
 
 const Navbar = () => {
   const { user, setUser } = useContext<any>(UserContext);
   return (
     <header className="w-full mx-auto p-4 sm:px-20 fixed top-0 z-50 shadow border-b-2">
       <div className="justify-between md:items-center md:flex">
-        <h1 className="text-4xl">PrepMeister</h1>
+        <h1 className="text-4xl">
+          <Link href={"/"}>PrepMeister</Link>
+        </h1>
         <div>
-          <Link href={'/problems'}>Problems</Link>
+          <Link href={"/problems"}>Problems</Link>
         </div>
         {/* <div>Problems</div> */}
         <div>Discussion</div>
         <div>Groups</div>
-        {user ? (
-          <div>
-            <p>name : {!user.username ? "no" : user.username}</p>
-            <p>email : {user.email ? user.email : "nope"} </p>
+
+        {user && (
+          <div className="cursor-pointer group relative" id="pr">
+            <TbUserCircle width={100} height={100} />
+            
+            <div
+              className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
+								z-40 group-hover:scale-100 scale-0 
+								transition-all duration-300 ease-in-out"
+            >
+              <p className="text-sm">{user.username}</p>
+              <p>{user.email}</p>
+              <Link href={"/profile"}>
+                
+              <p>Go to Profile</p>
+              </Link>
+            </div>
           </div>
-        ) : (
-          "no"
         )}
       </div>
     </header>
--- app/problems/page.tsx
+++ app/problems/page.tsx
@@ -2,24 +2,33 @@
 import React from "react";
 import axios from "axios";
 import { useState,useEffect } from "react";
+import slugify from "slugify";
+import Link from "next/link";
+import { BsCheckCircle } from "react-icons/bs";
+import { AiFillYoutube } from "react-icons/ai";
+import { IoClose } from "react-icons/io5";
+// import YouTube from "react-youtube";
+
 
 interface ProblemType {
   title:string;
   description:string;
   number:number;
+  tags:Array<string>;
+  comapany:Array<string>;
+  level:string;
 }
 
 const Problems = () => {
-  const [data,setData] = useState<ProblemType>()
-  let ps:ProblemType|[] = []
-  const perPage = 1;
+  const [data,setData] = useState<ProblemType[]>()
+  let ps:ProblemType[]
+  const lim = 4;
   const getProblems = async () => {
-    console.log('data')
     try {
-      const d = await axios.post("/api/problem/get", { perPage });
+      const d = await axios.post("/api/problem/get", { lim });
       ps = d.data.data
-      console.log(ps)
-      // setData(data?.title:d.data.title)
+      setData(d.data.data)
+      // console.log(data && data[0].title)
     } catch (error: any) {
       console.log(error.message);
     }
@@ -29,15 +38,72 @@ const Problems = () => {
     },[])
 
   return (
-    <div>
-      <div className="m-48 text-center text-3xl font-bold">
-        <p>Problems</p>
-      </div>
-      <div className="text-center">
-        {/* {ps.length>0 && ps?.map((problem,ind)=>{
-          <div key={ind}></div>
-        })} */}
-      </div>
+    // <div>
+    //   <div className="mt-48 p-4 text-center text-3xl font-bold">
+    //     <p>Problems</p>
+    //   </div>
+    //   <div className="text-lg">
+    //     {data && data.map((problem,id)=>{
+    //       return (
+    //         <ul key={id} className="flex flex-col m-4 p-4 space-x-4">
+    //           <li> {problem.number}</li>
+    //           <li> {problem.title}</li>
+    //           <li> {problem.description}</li>
+    //         </ul>
+    //       );
+    //     })}
+    //   </div>
+    // </div>
+
+    <div className="m-32 flex justify-center mx-auto">
+        <table className="w-full text-center space-x-10">
+        <thead className="text-lg font-bold">
+          <tr>
+            <td>Number</td>
+            <td>Title</td>
+            <td>Tags</td>
+            <td>Difficulty</td>
+          </tr>
+
+        </thead>
+        <tbody>
+
+        {data && data.map((problem, idx) => {
+          const difficulyColor =
+          problem.level === "easy"
+          ? "text-dark-green-s"
+          : problem.level === "medium"
+          ? "text-dark-yellow"
+              : "text-dark-red";
+          return (
+            <tr
+              key={idx}
+              className={`${problem.number % 2 == 1 ? "bg-dark-layer-1" : ""}`}
+            >
+              <td>{problem.number}</td>
+              <td>
+                <Link href={`/problems/${slugify(problem.title)}`}>
+                {problem.title}
+                </Link>
+                </td>
+              <td>
+                {problem.tags.map((tag, id) => {
+                  return (
+                    <span key={id} className="p-2 bg-slate-500/70 m-1 rounded">
+                      {tag}
+                    </span>
+                  );
+                })}
+              </td>
+              <td className={`${difficulyColor}`}>
+                {problem.level ? problem.level : "medium"}
+              </td>
+            </tr>
+          );
+          })}
+          </tbody>
+        </table>
+      
     </div>
   );
 };
