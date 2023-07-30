e59cc2991405c1ae24ebb05afb4c99d2fc2f7984
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-07-30 23:58:13 +0530

    bug check

--- app/api/users/getSolved/route.ts
+++ app/api/users/getSolved/route.ts
@@ -2,7 +2,7 @@ import { connect } from "@/dbConfig/dbConfig";
 import { NextRequest,NextResponse } from "next/server";
 import User from "@/models/userModel";
 
-connect()
+connect();
 
 export async function POST(req:NextRequest){
     try{
