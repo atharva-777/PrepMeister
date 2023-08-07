d33c58107457a219467ff77d3bd134988f7e1bbb
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-08-07 23:55:27 +0530

    added dummy testcases input-output

--- app/components/Workspace/Playground/Playground.tsx
+++ app/components/Workspace/Playground/Playground.tsx
@@ -1,49 +1,74 @@
-import React from 'react'
+import React from "react";
 import CodeMirror from "@uiw/react-codemirror";
-import { javascript } from '@codemirror/lang-javascript';
-import { oneDarkTheme } from '@codemirror/theme-one-dark';
-import { cpp } from '@codemirror/lang-cpp';
-import Split from 'react-split';
-import Preference from './PreferenceBar/Preference';
+import { javascript } from "@codemirror/lang-javascript";
+import { oneDarkTheme } from "@codemirror/theme-one-dark";
+import { cpp } from "@codemirror/lang-cpp";
+import Split from "react-split";
+import Preference from "./PreferenceBar/Preference";
 
-type PlaygroundProps = {
+type PlaygroundProps = {};
 
-}
+const Playground: React.FC<PlaygroundProps> = ({}) => {
 
-const Playground:React.FC<PlaygroundProps> = ({}) => {
 
-    const language = 'javascript';
+    const handleSubmit = () => {
+         
+    }
 
-    return (
-      <div className=" bg-black text-white">
-        <Preference />
-        <Split
-          className="h-[calc(100vh-94px)]"
-          direction="vertical"
-          sizes={[60, 40]}
-          minSize={60}
-        >
-          <div className="w-full overflow-auto">
-            <CodeMirror
-              value="const a = 1;"
-              theme={oneDarkTheme}
-              extensions={[javascript()]}
-              style={{ fontSize: 16 }}
-            />
-            {/* <CodeMirror value='double d = 5.2;' theme={oneDarkTheme} extensions={[cpp()]} style={{fontSize:18}}/> */}
-          </div>
-          <div className='w-full px-5 overflow-auto'>
-            <div className="flex h-10 items-center space-x-6">
-                <div className="relative flex h-full justify-center cursor-pointer">
-                    <div className="text-sm font-medium leading-5 text-white">Testcases
-                    <hr />
-                    </div>
-                </div>
+  return (
+    <div className="flex flex-col bg-black text-white relative overflow-x-hidden">
+      <Preference />
+      <Split
+        className="h-[calc(100vh-94px)]"
+        direction="vertical"
+        sizes={[60, 40]}
+        minSize={60}
+      >
+        <div className="w-full overflow-auto">
+          <CodeMirror
+            value="const a = 1;"
+            theme={oneDarkTheme}
+            extensions={[javascript()]}
+            style={{ fontSize: 16 }}
+          />
+        </div>
+
+        <div className="w-full px-5 overflow-auto">
+          <div className="flex h-10 items-center space-x-6">
+            <div className="relative flex h-full flex-col justify-center cursor-pointer">
+              <div className="text-sm font-medium leading-5 text-white">
+                Testcases
+              </div>
+              <hr className="absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white" />
             </div>
           </div>
-        </Split>
-      </div>
-    );
-}
 
-export default Playground
+          <div className="flex">
+            <div className="mr-2 items-start mt-2 ">
+              <div className="flex flex-wrap items-center gap-y-4">
+                <div
+                  className={`font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap`}
+                >
+                  Case 1
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="font-semibold my-4">
+            <p className="text-sm font-medium mt-4 text-white">Input:</p>
+            <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
+              atharva
+            </div>
+            <p className="text-sm font-medium mt-4 text-white">Output:</p>
+            <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
+              not palindrome
+            </div>
+          </div>
+        </div>
+      </Split>
+    </div>
+  );
+};
+
+export default Playground;
