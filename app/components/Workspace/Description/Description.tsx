f847f36e1c0aadff98bc61670230ac9cbaac0dd2
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-09-15 23:47:22 +0530

    updated description page

--- app/components/Workspace/Description/Description.tsx
+++ app/components/Workspace/Description/Description.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FiThumbsDown, FiThumbsUp } from "react-icons/fi";
+import { TbCircleCheckFilled } from "react-icons/tb";
 
 type DescriptionProps = {
   problem: ProblemType;
@@ -8,48 +10,44 @@ const Description: React.FC<DescriptionProps> = ({ problem }) => {
   return (
     <div className="h-screen overflow-auto flex-1">
       <div className="text-md m-2 font-sans font-light text-start">
-        <ul>
-          <li className="text-xl font-bold">
-            <span>
-              {problem.number}
-              {". "}
-            </span>
-            {problem.title}
-          </li>
-          <li>{problem.description}</li>
-          <li>{problem.level}</li>
-          <li>{problem.slug}</li>
-          <li>{problem._id}</li>
-          <li>{problem.tags[0]}</li>
-          <li>{problem.company[0]}</li>
+        {/* overall description */}
+        <div className="flex flex-row">
+          {/* title */}
+          <div className="text-xl hover:font-bold font-medium flex flex-row justify-end">
+            <div>
+              {problem.number} {problem.title}
+            </div>
+            <div>
+              <TbCircleCheckFilled size={10} />
+            </div>
+          </div>
+          {/* Description */}
+          <div className="font-sm text-neutral-700">
+            {problem.description}
+          </div>
 
-          <li>{problem.description}</li>
-          <li>{problem.level}</li>
-          <li>{problem.slug}</li>
-          <li>{problem._id}</li>
-          <li>{problem.tags[0]}</li>
-          <li>{problem.company[0]}</li>
-        </ul>
-        <div className="flex flex-col justify-between">
-          <h3 className="text-center m-2 text-lg font-bold">Examples</h3>
-          <div>
-            {problem.examples &&
-              problem.examples.map((example, key) => {
-                return (
-                  <div
-                    className=" border shadow-lg bg-gray-200/100 m-3 p-4"
-                    key={key}
-                  >
-                    <h2 className="">Example No : {key + 1}</h2>
-                    <ul>
-                      <li>Input : {example.input}</li>
-                      <li>Output : {example.output}</li>
+          {/* examples */}
+          <div className="flex flex-col justify-between">
+            <h3 className="text-center m-2 text-lg font-bold">Examples</h3>
+            <div>
+              {problem.examples &&
+                problem.examples.map((example, key) => {
+                  return (
+                    <div
+                      className=" border shadow-lg bg-gray-200/100 m-3 p-4"
+                      key={key}
+                    >
+                      <h2 className="">Example No : {key + 1}</h2>
+                      <ul>
+                        <li>Input : {example.input}</li>
+                        <li>Output : {example.output}</li>
 
-                      <li>Explaination : {example.explaination}</li>
-                    </ul>
-                  </div>
-                );
-              })}
+                        <li>Explaination : {example.explaination}</li>
+                      </ul>
+                    </div>
+                  );
+                })}
+            </div>
           </div>
         </div>
       </div>
