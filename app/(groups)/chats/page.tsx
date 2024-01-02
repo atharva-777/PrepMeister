a089b3eb5e61b6886cff3e47338971e029b1aff4
Atharva Jadhav <jadhavatharva499@gmail.com>
2024-01-02 23:27:46 +0530

    chat page

--- app/(groups)/chats/page.tsx
+++ app/(groups)/chats/page.tsx
@@ -1,61 +1,80 @@
 "use client";
 import React, { useState } from "react";
 import useWebSocket from "@/helper/useWebSocket";
+import Avatar from "react-avatar";
+
+const chats = [
+  {
+    name: "Dynamic Programming Questions asked in FAANG",
+    email: "jadhavatharva499@gmail.com",
+    lastSeen: "yesterday",
+    role: "admin",
+    imageUrl: "",
+  },
+  {
+    name: "Upsolving Contest Problems",
+    email: "niks@gmail.com",
+    lastSeen: "today",
+    role: "member",
+    imageUrl: "",
+  },
+  {
+    name: "Flipkart Grid 5.0 OA Questions",
+    email: "adi09x@gmail.com",
+    lastSeen: "4Hrs Ago",
+    role: "member",
+    imageUrl: "",
+  },
+];
 
 const Chats = () => {
-  const { messages, sendMessage, error } = useWebSocket();
+  // const { messages, sendMessage, error } = useWebSocket();
   const [inputMessage, setInputMessage] = useState<string>("");
 
-  console.log("messages , error ", { messages, error });
-
-  if (error) {
-    return <div>Error : {error}</div>;
-  }
+  // if (error) {
+  //   return <div>Error : {error}</div>;
+  // }
 
   return (
     <div className="p-24">
-      You have not joined any chats yet.
-      <br />
-      Please join chats to see here!
-      <div className="m-4 p-2 space-y-6">
-        <div className="p-3">
-          <ul>
-            Listing all previous messages :{" "}
-            {messages.map((message, ind) => (
-              <li key={ind}>{message}</li>
-            ))}{" "}
-          </ul>
-        </div>
-
-        <form
-          action=""
-          onSubmit={(event) => {
-            event.preventDefault();
-            sendMessage(inputMessage);
-          }}
-        >
-          <input
-            type="text"
-            name="message"
-            className="border border-black/25 m-2"
-            onChange={(e) => {
-              setInputMessage(e.target.value);
-              // console.log("Input msg ", inputMessage, e.target.value);
-            }}
-          />
-          <button
-            type="submit"
-            className="rounded-md bg-green-400 p-2"
-            onClick={(e) => {
-              e.preventDefault();
-              console.log("input ", inputMessage);
-              sendMessage(inputMessage);
-            }}
-          >
-            Send
-          </button>
-        </form>
-      </div>
+      <ul role="list" className="divide-y divide-gray-100">
+        {chats.map((person) => (
+          <li key={person.email} className="flex justify-between gap-x-6 py-5">
+            <div className="flex min-w-0 gap-x-4">
+              <Avatar
+                className="h-12 w-12 flex-none rounded-full bg-gray-50"
+                size="40px"
+                name={person.name}
+                color={"orange"}
+              />
+              <div className="min-w-0 flex-auto">
+                <p className="text-sm font-semibold leading-6 text-gray-900">
+                  {person.name}
+                </p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                  {person.email}
+                </p>
+              </div>
+            </div>
+            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+              <p className="text-sm leading-6 text-gray-900">{person.role}</p>
+              {person.lastSeen ? (
+                <p className="mt-1 text-xs leading-5 text-gray-500">
+                  Last Message{" "}
+                  <time dateTime={person.lastSeen}>{person.lastSeen}</time>
+                </p>
+              ) : (
+                <div className="mt-1 flex items-center gap-x-1.5">
+                  <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                  </div>
+                  <p className="text-xs leading-5 text-gray-500">Online</p>
+                </div>
+              )}
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useContext } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import Link from "next/link";
+import Avatar from "react-avatar";
 import { signOut, useSession } from "next-auth/react";
 
 const UserProfile = () => {
@@ -10,21 +11,26 @@ const UserProfile = () => {
   const user = session?.user;
   const router = useRouter();
 
+  if(!user){
+    router.push('/');
+  }
+
   return (
+    user && 
     <div>
-      <div className="text-center place-items-center place-content-center justify-center mt-48">
-        <div>
-          <h1 className="text-4xl m-4"> Welcome to Profile </h1>
-          <h2>
-            {!user ? (
-              "Nothing"
-            ) : (
-              <Link href={`/`}>Go to Profile</Link>
-            )}
-          </h2>
+      <div className="text-center flex flex-col items-center justify-center h-screen p-24">
+        <div className="space-y-3">
+          <h1 className="text-4xl m-4"> Welcome to Dashboard </h1>
+          <h2>{!user ? "Nothing" : <Link href={`/`}>Go to Profile</Link>}</h2>
+          <Avatar
+            className="h-24 w-24 flex-none rounded-full bg-gray-50"
+            size="40px"
+            name={user?.name || "K"}
+            color={"orange"}
+          />
         </div>
 
-        <div className="text-center m-8 bg-blue-200 max-w-2xl p-4 rounded-full mx-auto">
+        <div className="text-center m-8 bg-blue-200 max-w-2xl p-6 rounded-md mx-auto">
           <h3>User Details</h3>
           {user ? (
             <ul>
@@ -39,9 +45,8 @@ const UserProfile = () => {
           className="p-3 m-4 bg-red-400 rounded hover:bg-red-500 hover:z-20"
           onClick={(e) => {
             signOut();
-            router.push('/');
-          }
-          }
+            router.push("/");
+          }}
         >
           Logout
         </button>
